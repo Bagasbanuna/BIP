9cf7ae1c2d4a52164f9fdb2599e365a7724da36b
Bagas Banuna <bagasbanuna12@gmail.com>
2022-11-28 16:28:21 +0800

    push ke main

--- pages/api/api-create-profile.js
+++ pages/api/api-create-profile.js
@@ -3,19 +3,23 @@ const prisma = new PrismaClient();
 
 const ApiCreateProfile = async (req, res) => {
     if (req.method == 'POST') {
-        let body = JSON.parse(req.body)
-        console.log(body)
 
-        // res.status(200).json(body)}
-        let prof = await prisma.profile.create({
-            data: {
-                namaDepan: body.namaDepan,
-                namaBelakang: body.namaBelakang,
-                // Date dari FE akan dirubah format nya di server jika di tambah new Date, dan masuk ke DB sesuai dengan format Prisma
-                tanggalLahir: new Date(body.tanggalLahir),
-            }
-        })
-        res.status(200).json(prof)
+        let body = req.body
+        console.log(body)
+        res.status(200).json(body)
+
+        // let body = JSON.parse(req.body)
+        // console.log(body)
+
+        // let prof = await prisma.profile.create({
+        //     data: {
+        //         namaDepan: body.namaDepan,
+        //         namaBelakang: body.namaBelakang,
+        //         // Date dari FE akan dirubah format nya di server jika di tambah new Date, dan masuk ke DB sesuai dengan format Prisma
+        //         tanggalLahir: new Date(body.tanggalLahir),
+        //     }
+        // })
+        // res.status(200).json(prof)
     }
 }
 
